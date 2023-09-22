4e2380ef40506b933486e246729175311c82668a
vitor.rodrigues <vitor.rodrigues@cenpros.com.br>
2023-09-22 09:36:59 -0300

    Aplicacao de maps integrada com googleCloud

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MapPage from "./pages/MapPages";
+
+
+function App() {
+  return (
+    <MapPage/>
+  );
+}
+
+export default App;
